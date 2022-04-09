dab3463c61b8bc018748f390978a2d78c5dee1f0
Arthur Dennetiere <arthur.dennetiere@gmail.com>
2022-04-09 14:38:27 +0200

    add admin controller & configuration

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace stive.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
