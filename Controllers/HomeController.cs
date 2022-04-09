bdd6bd5bdcf9f9a57b5da79cbfdd359f65bcf9da
Maxence VANDEKERCKHOVE <maxence.vandekerckhove@viacesi.fr>
2022-04-09 19:31:06 +0200

    Adding loging and session system

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using stive.Models;
 using System;
@@ -20,6 +21,7 @@ namespace stive.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.sessionv = HttpContext.Session.GetString("Connected");
             return View();
         }
 
